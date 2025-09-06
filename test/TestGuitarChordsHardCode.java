31025130d904b3f6bd758a21044f7e69c908edf3
Nylon <jimmyfan20062006@gmail.com>
2025-09-06 14:40:52 +0800

    硬编码和弦，确定了扫弦方法没问题

---
+++ test/TestGuitarChordsHardCode.java
@@ -0,0 +1,28 @@
+package test;
+import util.*;
+import constant.*;
+public class TestGuitarChordsHardCode {
+    public static void main(String[] args) throws Exception {
+        Guitar guitar = new Guitar(GMInstruments.GUITAR_ACOUSTIC_NYLON);
+        
+        // --- 测试 C Major ---
+        System.out.println("播放: C Major (使用标准指法 x32010)");
+        guitar.reset();
+        guitar.guitarStrings[4].setFret(3); // 5弦3品
+        guitar.guitarStrings[3].setFret(2); // 4弦2品
+        guitar.guitarStrings[1].setFret(1); // 2弦1品
+        // 3弦和1弦是0品，reset后已经是了
+        // 6弦不弹，我们通过 strum 的起始点控制
+        guitar.strum(4, 2000, 100); // 从5弦(索引4)开始扫
+        Thread.sleep(2500);
+
+        // --- 测试 G Major ---
+        System.out.println("播放: G Major (使用标准指法 320003)");
+        guitar.reset();
+        guitar.guitarStrings[5].setFret(3); // 6弦3品
+        guitar.guitarStrings[4].setFret(2); // 5弦2品
+        guitar.guitarStrings[0].setFret(3); // 1弦3品
+        guitar.strum(5, 2000, 100); // 从6弦(索引5)开始扫
+        Thread.sleep(2500);
+    }
+}
