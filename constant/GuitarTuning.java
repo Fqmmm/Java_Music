b66ae6629a22e5d24caef5fed95c69d6c7909a34
Nylon <jimmyfan20062006@gmail.com>
2025-09-06 19:08:53 +0800

    新增吉他调弦信息（目前只有标准调弦）

---
+++ constant/GuitarTuning.java
@@ -0,0 +1,14 @@
+package constant;
+
+import model.MusicDraft;
+
+public class GuitarTuning {
+    public static int[] STANDARD_TUNING =  {
+        MusicDraft.low[3],    // E2 (6弦)
+        MusicDraft.low[6],    // A2 (5弦)
+        MusicDraft.medium[2], // D3 (4弦)
+        MusicDraft.medium[5], // G3 (3弦)
+        MusicDraft.medium[7], // B3 (2弦)
+        MusicDraft.high[3]    // E4 (1弦)
+    };
+}
