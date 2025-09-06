eb052b538986c2f4ebbe1162687df2587d1e3fd6
Nylon <jimmyfan20062006@gmail.com>
2025-09-06 09:14:43 +0800

    重新测试

--- test/TestTranspose.java
+++ test/TestTranspose.java
@@ -11,7 +11,7 @@ public class TestTranspose {
         花海 花海draft = new 花海();
         Music 花海 = 花海draft.getMusic(80);
         花海.setSinger("周杰伦");
-        player.playMusic(花海.transposed(-1));
+        player.playMusic(花海.transposed(3));
         player.close();
     }
 }
